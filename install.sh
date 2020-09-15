ca6e44a2c5daef20b2043a7042aea7eb3db43fc1
Hacker 4 Help <67947813+hacker4help@users.noreply.github.com>
2020-09-15 12:37:01 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,10 +10,12 @@ git clone https://github.com/projectdiscovery/nuclei-templates $dir/nuclei-templ
 GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/cmd/subfinder
 GO111MODULE=on go get -u -v github.com/projectdiscovery/httpx/cmd/httpx
 git clone https://github.com/defparam/smuggler $dir/smuggler
+cp nuclear.sh $dir/
 
 
 echo '
 alias nuclei=/root/go/bin/nuclei
 alias httpx=/root/go/bin/httpx
 alias subfinder=/root/fo/bin/subfinder
+alias nuclear=/root/Tools/nuclear.sh
 '>> ~/.bashrc

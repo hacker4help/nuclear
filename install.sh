04c38901b24d8525456ad0ce5b086e123dc12eea
Hacker 4 Help <67947813+hacker4help@users.noreply.github.com>
2020-09-15 15:19:17 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ echo 'Making Directories'
 dir=/root/Tools
 mkdir $dir
 echo 'Installing Tools'
-GO111MODULE=on go get -u -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+git clone https://github.com/projectdiscovery/nuclei.git; cd nuclei/cmd/nuclei/; go build; mv nuclei /usr/local/bin/; nuclei -h
 git clone https://github.com/projectdiscovery/nuclei-templates $dir/nuclei-templates
 GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/cmd/subfinder
 GO111MODULE=on go get -u -v github.com/projectdiscovery/httpx/cmd/httpx
@@ -14,7 +14,7 @@ cp nuclear.sh $dir/
 
 
 echo '
-alias nuclei=/root/go/bin/nuclei
+
 alias httpx=/root/go/bin/httpx
 alias subfinder=/root/fo/bin/subfinder
 alias nuclear=/root/Tools/nuclear.sh

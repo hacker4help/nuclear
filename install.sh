454cd1192c7b647757bddaaa2420710aebb1713a
Hacker 4 Help <67947813+hacker4help@users.noreply.github.com>
2020-09-15 16:42:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,16 +7,17 @@ mkdir $dir
 echo 'Installing Tools'
 git clone https://github.com/projectdiscovery/nuclei.git; cd nuclei/cmd/nuclei/; go build; mv nuclei /usr/local/bin/; nuclei -h
 git clone https://github.com/projectdiscovery/nuclei-templates $dir/nuclei-templates
-GO111MODULE=on go get -v github.com/projectdiscovery/subfinder/cmd/subfinder
-GO111MODULE=on go get -u -v github.com/projectdiscovery/httpx/cmd/httpx
+git clone https://github.com/projectdiscovery/subfinder.git
+cd subfinder/cmd/subfinder
+go build .
+mv subfinder /usr/local/bin/
+subfinder -h
+git clone https://github.com/projectdiscovery/httpx.git; cd httpx/cmd/httpx; go build; mv httpx /usr/local/bin/; httpx -h
 git clone https://github.com/defparam/smuggler $dir/smuggler
 cp nuclear.sh $dir/
 
 
 echo '
-
-alias httpx=/root/go/bin/httpx
-alias subfinder=/root/go/bin/subfinder
 alias nuclear=/root/Tools/nuclear.sh
 '>> ~/.bashrc
 
